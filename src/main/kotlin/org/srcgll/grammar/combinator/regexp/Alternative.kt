e9a091b1f6da6d31e4051650821bf09bc7401394
Olga Bachishe <bachisheo@yandex.ru>
2023-12-07 14:42:51 +0300

    Add `option` and `some`

--- src/main/kotlin/org/srcgll/grammar/combinator/regexp/Alternative.kt
+++ src/main/kotlin/org/srcgll/grammar/combinator/regexp/Alternative.kt
@@ -2,16 +2,13 @@ package org.srcgll.grammar.combinator.regexp
 
 
 data class Alternative
-(
-    internal val left  : Regexp,
-    internal val right : Regexp,
-)
-    : Regexp
-{
+    (
+    internal val left: Regexp,
+    internal val right: Regexp,
+) : Regexp {
     companion object {
-        fun makeAlternative(left : Regexp, right : Regexp) : Regexp
-        {
-            if (left is Empty)  return right
+        fun makeAlternative(left: Regexp, right: Regexp): Regexp {
+            if (left is Empty) return right
             if (right is Empty) return left
 
             if (left is Alternative && (right == left.left || right == left.right)) {
@@ -22,22 +19,22 @@ data class Alternative
             }
             return if (left == right) left else Alternative(left, right)
         }
+
+        fun makeAlternative(literals: Iterable<String>): Regexp {
+            val terms = literals.map { Term(it) }
+            val initial: Regexp = terms[0] or terms[1]
+
+            return terms.subList(2, terms.size)
+                .fold(initial) { acc: Regexp, i: Term<String> -> Alternative.makeAlternative(acc, i) }
+        }
     }
 
-    override fun derive(symbol : DerivedSymbol) : Regexp
-    {
+    override fun derive(symbol: DerivedSymbol): Regexp {
         return makeAlternative(left.derive(symbol), right.derive(symbol))
     }
 
 }
 
-infix fun Regexp.or(other : Regexp) : Regexp = Alternative.makeAlternative(left = this, other)
+infix fun Regexp.or(other: Regexp): Regexp = Alternative.makeAlternative(left = this, other)
 
-fun makeAlternative(literals : Iterable<String>) : Regexp
-{
-    val terms = literals.map { Term(it) }
-    val initial : Regexp = terms[0] or terms[1]
-
-    return terms.subList(2, terms.size)
-        .fold(initial) { acc : Regexp, i : Term<String> -> Alternative.makeAlternative(acc, i) }
-}
+fun opt(exp: Regexp): Regexp = Alternative.makeAlternative(exp, Epsilon)
