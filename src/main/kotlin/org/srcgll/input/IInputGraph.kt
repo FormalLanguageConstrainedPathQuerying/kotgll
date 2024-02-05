a33401a785da853c1fe3d5225f7246aec7891df3
Olga Bachishe <bachisheo@yandex.ru>
2024-02-05 16:17:16 +0300

    Refactor: separate edges handling from gll core

--- src/main/kotlin/org/srcgll/input/IInputGraph.kt
+++ src/main/kotlin/org/srcgll/input/IInputGraph.kt
@@ -1,10 +1,12 @@
 package org.srcgll.input
 
+import org.srcgll.RecoveryMode
 import org.srcgll.descriptors.Descriptor
 import org.srcgll.parser.context.IContext
 import org.srcgll.rsm.RsmState
 import org.srcgll.rsm.symbol.Nonterminal
 import org.srcgll.rsm.symbol.Terminal
+import org.srcgll.sppf.TerminalRecoveryEdge
 import org.srcgll.sppf.node.SppfNode
 
 interface IInputGraph<VertexType, LabelType : ILabel> {
@@ -30,11 +32,11 @@ interface IInputGraph<VertexType, LabelType : ILabel> {
     fun handleEdges(
         handleTerminalOrEpsilonEdge: (
             curDescriptor: Descriptor<VertexType>,
+            curSppfNode: SppfNode<VertexType>?,
             terminal: Terminal<*>?,
             targetState: RsmState,
-            sppfNode: SppfNode<VertexType>?,
             targetVertex: VertexType,
-            weight: Int
+            targetWeight: Int,
         ) -> Unit,
         handleNonterminalEdge: (
             descriptor: Descriptor<VertexType>,
@@ -44,32 +46,122 @@ interface IInputGraph<VertexType, LabelType : ILabel> {
         ) -> Unit,
         ctx: IContext<VertexType, LabelType>,
         curDescriptor: Descriptor<VertexType>,
-        sppfNode: SppfNode<VertexType>?
+        curSppfNode: SppfNode<VertexType>?
     ) {
-        val pos = curDescriptor.inputPosition
         val state = curDescriptor.rsmState
-
-        for ((edgeNonterminal, targetStates) in curDescriptor.rsmState.getNonterminalEdges()) {
-            handleNonterminalEdge(curDescriptor, edgeNonterminal, targetStates, sppfNode)
-        }
-
+        val pos = curDescriptor.inputPosition
         val gssNode = curDescriptor.gssNode
-        for (inputEdge in getEdges(pos)) {
+        val terminalEdges = state.getTerminalEdges()
+        val nonterminalEdges = state.getNonterminalEdges()
+        for (inputEdge in ctx.input.getEdges(pos)) {
             if (inputEdge.label.terminal == null) {
-                handleTerminalOrEpsilonEdge(
-                    curDescriptor, null, state, sppfNode, inputEdge.head, 0
+                val descriptor = Descriptor(
+                    state, gssNode, ctx.sppf.getParentNode(
+                        state, curSppfNode, ctx.sppf.getOrCreateTerminalSppfNode(
+                            terminal = null, pos, inputEdge.head
+                        )
+                    ), inputEdge.head
                 )
+                ctx.addDescriptor(descriptor)
                 continue
             }
-            for ((edgeTerminal, targetStates) in state.getTerminalEdges()) {
+            for ((edgeTerminal, targetStates) in terminalEdges) {
                 if (inputEdge.label.terminal == edgeTerminal) {
                     for (target in targetStates) {
+                        handleTerminalOrEpsilonEdge(curDescriptor, curSppfNode, edgeTerminal, target, inputEdge.head, 0)
+                    }
+                }
+            }
+        }
+
+        for ((edgeNonterminal, targetStates) in nonterminalEdges) {
+            handleNonterminalEdge(curDescriptor, edgeNonterminal, targetStates, curSppfNode)
+        }
+
+        if (ctx.recovery == RecoveryMode.ON) {
+            val errorRecoveryEdges = createRecoveryEdges(curDescriptor)
+            handleRecoveryEdges(errorRecoveryEdges, handleTerminalOrEpsilonEdge, curDescriptor, terminalEdges)
+        }
+    }
+
+    private fun createRecoveryEdges(curDescriptor: Descriptor<VertexType>): HashMap<Terminal<*>?, TerminalRecoveryEdge<VertexType>> {
+        val pos = curDescriptor.inputPosition
+        val state = curDescriptor.rsmState
+        val terminalEdges = state.getTerminalEdges()
+
+
+        val errorRecoveryEdges = HashMap<Terminal<*>?, TerminalRecoveryEdge<VertexType>>()
+        val currentEdges = getEdges(pos)
+        if (currentEdges.isNotEmpty()) {
+            for (currentEdge in currentEdges) {
+                if (currentEdge.label.terminal == null) continue
+
+                val currentTerminal = currentEdge.label.terminal!!
+
+                val coveredByCurrentTerminal: HashSet<RsmState> = terminalEdges[currentTerminal] ?: hashSetOf()
+
+                for (terminal in state.errorRecoveryLabels) {
+                    val coveredByTerminal = HashSet(terminalEdges[terminal] as HashSet<RsmState>)
+
+                    coveredByCurrentTerminal.forEach { coveredByTerminal.remove(it) }
+
+                    if (terminal != currentTerminal && coveredByTerminal.isNotEmpty()) {
+                        errorRecoveryEdges[terminal] = TerminalRecoveryEdge(pos, weight = 1)
+                    }
+                }
+
+                errorRecoveryEdges[null] = TerminalRecoveryEdge(currentEdge.head, weight = 1)
+            }
+        } else {
+            for (terminal in state.errorRecoveryLabels) {
+                if (!terminalEdges[terminal].isNullOrEmpty()) {
+                    errorRecoveryEdges[terminal] = TerminalRecoveryEdge(pos, weight = 1)
+                }
+            }
+        }
+        return errorRecoveryEdges
+    }
+
+    private fun handleRecoveryEdges(
+        errorRecoveryEdges: HashMap<Terminal<*>?, TerminalRecoveryEdge<VertexType>>,
+        handleTerminalOrEpsilonEdge: (
+            curDescriptor: Descriptor<VertexType>,
+            curSppfNode: SppfNode<VertexType>?,
+            terminal: Terminal<*>?,
+            targetState: RsmState,
+            targetVertex: VertexType,
+            targetWeight: Int,
+        ) -> Unit,
+        curDescriptor: Descriptor<VertexType>,
+        terminalEdges: HashMap<Terminal<*>, HashSet<RsmState>>
+    ) {
+        for ((terminal, errorRecoveryEdge) in errorRecoveryEdges) {
+            if (terminal == null) {
+                handleTerminalOrEpsilonEdge(
+                    curDescriptor,
+                    curDescriptor.sppfNode,
+                    null,
+                    curDescriptor.rsmState,
+                    errorRecoveryEdge.head,
+                    errorRecoveryEdge.weight
+                )
+            } else {
+
+                if (terminalEdges.containsKey(terminal)) {
+                    for (targetState in terminalEdges.getValue(terminal)) {
                         handleTerminalOrEpsilonEdge(
-                            curDescriptor, edgeTerminal, target, sppfNode, inputEdge.head, 0
+                            curDescriptor,
+                            curDescriptor.sppfNode,
+                            terminal,
+                            targetState,
+                            errorRecoveryEdge.head,
+                            errorRecoveryEdge.weight
                         )
                     }
                 }
             }
         }
+
     }
+
 }
--- src/main/kotlin/org/srcgll/parser/Gll.kt
+++ src/main/kotlin/org/srcgll/parser/Gll.kt
@@ -7,7 +7,6 @@ import org.srcgll.parser.context.IContext
 import org.srcgll.rsm.RsmState
 import org.srcgll.rsm.symbol.Nonterminal
 import org.srcgll.rsm.symbol.Terminal
-import org.srcgll.sppf.TerminalRecoveryEdge
 import org.srcgll.sppf.node.ISppfNode
 import org.srcgll.sppf.node.SppfNode
 import org.srcgll.sppf.node.SymbolSppfNode
@@ -59,103 +58,17 @@ class Gll<VertexType, LabelType : ILabel>(
 
         checkAcceptance(curSppfNode, leftExtent, rightExtent, state.nonterminal)
 
-        handleEdges(this::handleTerminalOrEpsilonEdge, this::handleNonterminalEdge, ctx, curDescriptor, curSppfNode)
+        ctx.input.handleEdges(
+            this::handleTerminalOrEpsilonEdge,
+            this::handleNonterminalEdge,
+            ctx,
+            curDescriptor,
+            curSppfNode
+        )
 
         if (state.isFinal) pop(gssNode, curSppfNode, pos)
     }
 
-    private fun handleEdges(
-        handleTerminalOrEpsilonEdge: (
-            curDescriptor: Descriptor<VertexType>,
-            curSppfNode: SppfNode<VertexType>?,
-            terminal: Terminal<*>?,
-            targetState: RsmState,
-            targetVertex: VertexType,
-            targetWeight: Int,
-        ) -> Unit,
-        handleNonterminalEdge: (
-            descriptor: Descriptor<VertexType>,
-            nonterminal: Nonterminal,
-            targetStates: HashSet<RsmState>,
-            curSppfNode: SppfNode<VertexType>?
-        ) -> Unit,
-        ctx: IContext<VertexType, LabelType>,
-        curDescriptor: Descriptor<VertexType>,
-        curSppfNode: SppfNode<VertexType>?
-    ) {
-        val state = curDescriptor.rsmState
-        val pos = curDescriptor.inputPosition
-        val gssNode = curDescriptor.gssNode
-        val terminalEdges = state.getTerminalEdges()
-        val nonterminalEdges = state.getNonterminalEdges()
-        for (inputEdge in ctx.input.getEdges(pos)) {
-            if (inputEdge.label.terminal == null) {
-                val descriptor = Descriptor(
-                    state, gssNode, ctx.sppf.getParentNode(
-                        state, curSppfNode, ctx.sppf.getOrCreateTerminalSppfNode(
-                            terminal = null, pos, inputEdge.head
-                        )
-                    ), inputEdge.head
-                )
-                addDescriptor(descriptor)
-                continue
-            }
-            for ((edgeTerminal, targetStates) in terminalEdges) {
-                if (inputEdge.label.terminal == edgeTerminal) {
-                    for (target in targetStates) {
-                        handleTerminalOrEpsilonEdge(curDescriptor, curSppfNode, edgeTerminal, target, inputEdge.head, 0)
-                    }
-                }
-            }
-        }
-
-        for ((edgeNonterminal, targetStates) in nonterminalEdges) {
-            handleNonterminalEdge(curDescriptor, edgeNonterminal, targetStates, curSppfNode)
-        }
-
-        if (ctx.recovery == RecoveryMode.ON) {
-            val errorRecoveryEdges = createRecoveryEdges(curDescriptor)
-            handleRecoveryEdges(errorRecoveryEdges, this::handleTerminalOrEpsilonEdge, curDescriptor, terminalEdges)
-        }
-    }
-
-    private fun createRecoveryEdges(curDescriptor: Descriptor<VertexType>): HashMap<Terminal<*>?, TerminalRecoveryEdge<VertexType>> {
-        val pos = curDescriptor.inputPosition
-        val state = curDescriptor.rsmState
-        val terminalEdges = state.getTerminalEdges()
-
-
-        val errorRecoveryEdges = HashMap<Terminal<*>?, TerminalRecoveryEdge<VertexType>>()
-        val currentEdges = ctx.input.getEdges(pos)
-        if (currentEdges.isNotEmpty()) {
-            for (currentEdge in currentEdges) {
-                if (currentEdge.label.terminal == null) continue
-
-                val currentTerminal = currentEdge.label.terminal!!
-
-                val coveredByCurrentTerminal: HashSet<RsmState> = terminalEdges[currentTerminal] ?: hashSetOf()
-
-                for (terminal in state.errorRecoveryLabels) {
-                    val coveredByTerminal = HashSet(terminalEdges[terminal] as HashSet<RsmState>)
-
-                    coveredByCurrentTerminal.forEach { coveredByTerminal.remove(it) }
-
-                    if (terminal != currentTerminal && coveredByTerminal.isNotEmpty()) {
-                        errorRecoveryEdges[terminal] = TerminalRecoveryEdge(pos, weight = 1)
-                    }
-                }
-
-                errorRecoveryEdges[null] = TerminalRecoveryEdge(currentEdge.head, weight = 1)
-            }
-        } else {
-            for (terminal in state.errorRecoveryLabels) {
-                if (!terminalEdges[terminal].isNullOrEmpty()) {
-                    errorRecoveryEdges[terminal] = TerminalRecoveryEdge(pos, weight = 1)
-                }
-            }
-        }
-        return errorRecoveryEdges
-    }
 
     private fun handleNonterminalEdge(
         descriptor: Descriptor<VertexType>,
@@ -224,46 +137,5 @@ class Gll<VertexType, LabelType : ILabel>(
     }
 
 
-    private fun handleRecoveryEdges(
-        errorRecoveryEdges: HashMap<Terminal<*>?, TerminalRecoveryEdge<VertexType>>,
-        handleTerminalOrEpsilonEdge: (
-            curDescriptor: Descriptor<VertexType>,
-            curSppfNode: SppfNode<VertexType>?,
-            terminal: Terminal<*>?,
-            targetState: RsmState,
-            targetVertex: VertexType,
-            targetWeight: Int,
-        ) -> Unit,
-        curDescriptor: Descriptor<VertexType>,
-        terminalEdges: HashMap<Terminal<*>, HashSet<RsmState>>
-    ) {
-        for ((terminal, errorRecoveryEdge) in errorRecoveryEdges) {
-            if (terminal == null) {
-                handleTerminalOrEpsilonEdge(
-                    curDescriptor,
-                    curDescriptor.sppfNode,
-                    null,
-                    curDescriptor.rsmState,
-                    errorRecoveryEdge.head,
-                    errorRecoveryEdge.weight
-                )
-            } else {
-
-                if (terminalEdges.containsKey(terminal)) {
-                    for (targetState in terminalEdges.getValue(terminal)) {
-                        handleTerminalOrEpsilonEdge(
-                            curDescriptor,
-                            curDescriptor.sppfNode,
-                            terminal,
-                            targetState,
-                            errorRecoveryEdge.head,
-                            errorRecoveryEdge.weight
-                        )
-                    }
-                }
-            }
-        }
-
-    }
 }
 
