0ce210504b353aeac8f7ef8368c9708c4a1305f8
cyb3r-b4stard <hollowcoder@yandex.ru>
2024-03-01 15:25:21 +0300

    gitignore update, removed resource files from tracking

--- out/jmh/resources/OpenJDKSourcesProcessed/ImplicitClassRecovery_processed.java
+++
@@ -1,7 +0,0 @@
-/**
- * @test /nodynamiccopyright/
- * @compile/fail/ref=ImplicitClassRecovery.out -XDrawDiagnostics --enable-preview --source ${jdk.version} ImplicitClassRecovery.java
- */
-public void main() {
-    System.err.println("Hello!")
-}
--- out/jmh/resources/OpenJDKSourcesProcessedErrorFree/Derr_processed.java
+++
@@ -1,5 +0,0 @@
-/* /nodynamiccopyright/ */
-
-@A(y=1) 
-class D {
-}
