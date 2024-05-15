cc32caad4fda19e7bcb5de16153afae258117db9
Olga Bachishe <bachisheo@yandex.ru>
2024-05-15 22:35:41 +0300

    update grammar

--- benchmarks/src/main/kotlin/org/Main.kt
+++ benchmarks/src/main/kotlin/org/Main.kt
@@ -1,5 +1,8 @@
 package org
 
+import org.antlr.Java8Lexer
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
 import org.ucfs.Java8
 import org.ucfs.JavaToken
 import org.ucfs.grammar.combinator.Grammar
@@ -8,6 +11,7 @@ import org.ucfs.grammar.combinator.extension.StringExtension.times
 import org.ucfs.grammar.combinator.regexp.Nt
 import org.ucfs.input.LinearInputLabel
 import org.ucfs.parser.ParserGenerator
+import java.io.File
 import java.nio.file.Path
 
 fun generate() {
@@ -34,11 +38,30 @@ fun runGenerated() {
         cnt += 1
     }
 }
-
+class Main(){}
 fun main() {
-    generate()
+   calculateTokens()
 }
 
+fun getAnrltTokens(file: String): Int{
+    val stream = CommonTokenStream(
+        Java8Lexer(
+            CharStreams.fromString(file)
+        )
+    )
+    return stream.numberOfOnChannelTokens
+}
+fun calculateTokens(){
+    val pth = Main::class.java.getResource("/junit")
+    val resourcesDir = File(pth.toURI())
+    val stat = File("junit-tokens.csv").printWriter()
+    stat.println("Param: fileName,Line Count")
+    for (file in resourcesDir.listFiles()!!){
+        // stat.println("${file.name},${getTokenStream(file.readText()).edges.size}")
+          stat.println("${file.name},${getAnrltTokens(file.readText())}")
+    }
+    stat.close()
+}
 fun run() {
     class L : Grammar() {
         val List by Nt().asStart()
