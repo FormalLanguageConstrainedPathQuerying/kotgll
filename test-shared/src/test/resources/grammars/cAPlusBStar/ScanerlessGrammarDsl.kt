1fd580296b5810fc4547344843c2e4b21e5d66bb
Olga Bachishe <bachisheo@yandex.ru>
2024-05-30 13:07:02 +0300

    fix tests

--- test-shared/src/test/resources/grammars/cAPlusBStar/ScanerlessGrammarDsl.kt
+++ test-shared/src/test/resources/grammars/cAPlusBStar/ScanerlessGrammarDsl.kt
@@ -1,12 +1,12 @@
 package grammars.cAPlusBStar
 
 import org.ucfs.grammar.combinator.Grammar
-import org.ucfs.grammar.combinator.regexp.Many
+import org.ucfs.grammar.combinator.regexp.many
 import org.ucfs.grammar.combinator.regexp.Nt
-import org.ucfs.grammar.combinator.regexp.Some
+import org.ucfs.grammar.combinator.regexp.some
 import org.ucfs.grammar.combinator.regexp.times
 import org.ucfs.rsm.symbol.Term
 
 class ScanerlessGrammarDsl : Grammar() {
-    val S by Nt(Term("c") * Some(Term("a")) * Many(Term("b"))).asStart()
+    val S by Nt(Term("c") * some(Term("a")) * many(Term("b"))).asStart()
 }
