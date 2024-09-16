f781dd45c6997aa985abb76bfdc134494a04018d
Olga Bachishe <bachisheo@gmail.com>
2024-09-16 09:02:51 +0300

    Add time out & error handling in benchmarks

--- benchmarks/src/test/kotlin/TimeParsingBenchmark.kt
+++ benchmarks/src/test/kotlin/TimeParsingBenchmark.kt
@@ -1,9 +1,12 @@
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.Timeout
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
+import java.time.Duration.ofSeconds
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import kotlin.io.path.name
@@ -14,12 +17,13 @@ abstract class TimeParsingBenchmark {
     private val repeatCount: Int = 1
     lateinit var file: File
 
+    private fun getFolderName() = "${getShortName()}_${getResourceFolder()}_$version.csv"
 
     private fun initFolder(): DynamicTest {
         val resultPath = Path.of("src", "test", "result", getShortName())
         return dynamicTest("initiation for ${getShortName()}") {
             Files.createDirectories(resultPath)
-            file = File(resultPath.toString(), "${getShortName()}_$version.csv")
+            file = File(resultPath.toString(), getFolderName())
             file.createNewFile()
             file.writeText("fileName,result(avg $repeatCount times)")
         }
@@ -32,10 +36,19 @@ abstract class TimeParsingBenchmark {
         var result: Long = 0
         for(i in 0..repeatCount) {
             val startTime = System.currentTimeMillis()
-            parse(text)
+            try{
+                parse(text)
+            }
+            catch (e: Exception){
+                report(fileName, e.toString())
+            }
             result += System.currentTimeMillis() - startTime
         }
         result /= repeatCount
+        report(fileName, result.toString())
+    }
+
+    private fun report(fileName: String, result: String){
         val message = "$fileName,$result"
         println(message)
         file.appendText("\n$message")
@@ -43,8 +56,8 @@ abstract class TimeParsingBenchmark {
 
     abstract fun parse(text: String)
 
-    private fun getResourceFolder(): String = Path.of("java", "correct", "junit-4-12")
-        .toString()
+    private fun getResourceFolder(): String = Path.of("test_for_test").toString()
+        //Path.of("java", "correct", "junit-4-12").toString()
 
 
     private fun getResource(resourceFolder: String): Path {
@@ -54,6 +67,7 @@ abstract class TimeParsingBenchmark {
     }
 
     @TestFactory
+    @Timeout(1)
     fun timeTest(): Collection<DynamicTest> {
         return getTests(getResource(getResourceFolder()), ::runTimeTest)
     }
@@ -62,7 +76,12 @@ abstract class TimeParsingBenchmark {
         return listOf(initFolder()) + Files.list(folder).map { file ->
             dynamicTest(file.fileName.toString()) {
                 val source = file.toFile().readText()
-                run(file.name, source)
+
+                Assertions.assertTimeoutPreemptively(
+                    ofSeconds(1)
+                ) { run(file.name, source) }
+
+                report(file.name, "timeout")
             }
         }.toList()
     }
