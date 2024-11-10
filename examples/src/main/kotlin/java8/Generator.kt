b0bc2bf6c0c3f6a6510fffdbdd1d8af6e59523f0
Olga Bachishe <bachisheo@gmail.com>
2024-11-10 21:46:15 +0300

    Added nodes generation in script

--- examples/src/main/kotlin/java8/Generator.kt
+++ examples/src/main/kotlin/java8/Generator.kt
@@ -1,29 +1,35 @@
 package java8
+import org.ucfs.ast.NodeClassesGenerator
 import org.ucfs.parser.ParserGenerator
 import org.ucfs.parser.RecoveryParserGenerator
 import java.nio.file.Path
-class Generator
+
+val grammarClass = Java8::class.java
+val tokenClass = JavaToken::class.java
+val pkg = "org.ucfs"
+
 fun main(args: Array<String>){
-//    if(args.size != 1){
-//        throw IllegalArgumentException("Set first argument as path to generation")
-//    }
-//    val path = Path.of(args[0])
-    val path = Path.of("benchmarks/src/main/kotlin")
+    val path: Path = if(args.isEmpty()){
+        Path.of("benchmarks/src/main/kotlin")
+    }
+    else{
+        Path.of(args[0])
+    }
     println("Generate ${Java8::class} UCFS parsers at ${path.toAbsolutePath()}")
     generateJavaParser(path)
     generateJavaRecoveryParser(path)
+    generateNodes(path)
 }
 
 fun generateJavaParser(path: Path) {
-    ParserGenerator(
-        Java8::class.java,
-        JavaToken::class.java
-    ).generate(path, "org.ucfs")
+    ParserGenerator(grammarClass, tokenClass).generate(path, pkg)
 }
 fun generateJavaRecoveryParser(path: Path) {
-    RecoveryParserGenerator(
-        Java8::class.java,
-        JavaToken::class.java
-    ).generate(path, "org.ucfs")
+    RecoveryParserGenerator(grammarClass, tokenClass).generate(path, pkg)
+}
+
+fun generateNodes(path: Path){
+    var nodeGenerator = NodeClassesGenerator(grammarClass)
+    nodeGenerator.generate(path, "$pkg.nodes")
 }
 
